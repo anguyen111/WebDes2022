934c39c1067bfdbdd16c2a69a04a46e68ff726cb
anguyen111 <100880135+anguyen111@users.noreply.github.com>
2022-03-03 14:19:43 -0800

    First push
    This is the first commit

---
+++ nguyenAddison-webSpring2022/_js/myJavaScript.js
@@ -0,0 +1,30 @@
+let myName = "Addison";
+console.log(myName);
+
+// this function logs a line of text in the browser
+// console is an object, has functions and properties
+console.log("hello world!");
+
+function init() {
+    changeHeading();
+    greeting();
+    changeImg();
+}
+
+// this creates a function
+function changeHeading() {
+    let myHeading = document.querySelector('h1');
+    myHeading = document.querySelector('h2');
+    myHeading.textContent = 'Hello world!';
+}
+
+let greetingPre = "Hello there, "
+
+function greeting() {
+    // let myGreeting = document.createElement('p');
+    // myGreeting.innerText = myName;
+    // document.body.appendChild(myGreeting)
+    let myGreeting = document.querySelector('p');
+    myGreeting.innerText = greetingPre + myName;
+}
+    
---
+++ nguyenAddison-webSpring2022/_js/rps.js
@@ -0,0 +1,77 @@
+let userChoice;
+let win;
+let choices = ["rock", "paper", "scissors"]; 
+
+
+function createRandom() {
+    let randnum = Math.floor(Math.random() * choices.length);
+    return cpu = choices[randnum];
+}
+
+function drawImg(pic, width, height, alt, id){
+    var x = document.createElement("IMG");
+    x.setAttribute("src", pic);
+    x.setAttribute("width", width);
+    x.setAttribute("height", height);
+    x.setAttribute("alt", alt);
+    document.getElementById(id).appendChild(x);
+}
+
+// gives userChoice value, creates image for computer input
+function choose(choice) {
+    userChoice = choice;
+    deleteImage("playerID");
+    deleteImage("comID");
+    deleteText();
+    createRandom();
+    createImage();
+    playRPS();
+}
+
+function createImage() {
+    if (userChoice == "rock") {
+        drawImg("http://images.clipartpanda.com/rock-clipart-7Ta7BALTA.png", "90", "120", "rock", "playerID")
+    } if (userChoice == "paper") {
+        drawImg("http://clipart-library.com/data_images/296148.png", "90", "120", "paper", "playerID")
+    } if (userChoice == "scissors"){
+        drawImg("https://www.pngkit.com/png/full/28-284006_black-and-white-scissors-free-clip-art-black.png", "90", "120", "scissors", "playerID")
+    } if (cpu == "rock") {
+        drawImg("http://images.clipartpanda.com/rock-clipart-7Ta7BALTA.png", "90", "120", "rock", "comID")
+    } if (cpu == "paper") {
+        drawImg("http://clipart-library.com/data_images/296148.png", "90", "120", "paper", "comID")
+    } if (cpu == "scissors"){
+        drawImg("https://www.pngkit.com/png/full/28-284006_black-and-white-scissors-free-clip-art-black.png", "90", "120", "scissors", "comID")
+    } 
+}
+// I got this from w3schools.com/jsref/tryit.asp?filename=tryjsref_node_removechild_while 
+function deleteImage(id) {
+    var element = document.getElementById(id);
+    while (element.hasChildNodes()) {
+        element.removeChild(element.firstChild);
+    }
+}
+// gives the result
+function playRPS() {
+    if (cpu == "rock" && userChoice == "rock" || cpu == "paper" && userChoice == "paper" || cpu == "scissors" && userChoice == "scissors"){
+        let winner = document.createElement('h1');
+        winner.innerText = "It's a tie";
+        document.getElementById("resultText").appendChild(winner);
+    }
+    else if (cpu == "paper" && userChoice == "rock" || cpu == "scissors" && userChoice == "paper" || cpu == "rock" && userChoice == "scissors") {
+        let winner = document.createElement('h1');
+        winner.innerText = "You lose";
+        document.getElementById("resultText").appendChild(winner);
+    }
+    else {
+        let winner = document.createElement('h1');
+        winner.innerText = "You win";
+        document.getElementById("resultText").appendChild(winner);
+    }
+}
+
+function deleteText() {
+    var element = document.getElementById("resultText");
+    while (element.hasChildNodes()) {
+        element.removeChild(element.firstChild);
+    }
+}
